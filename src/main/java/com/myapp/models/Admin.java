aefe65d2b8e29913d607a8dca60658c2d06a4cb6
ChJouamai <jouamaichaimae@gmail.com>
2024-05-24 01:27:35 +0100

    admin update

--- src/main/java/com/myapp/models/Admin.java
+++ src/main/java/com/myapp/models/Admin.java
@@ -1,19 +1,1 @@
-package com.myapp.models;
 
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
-
-@Entity
-@DiscriminatorValue("ADMIN")
-public class Admin extends Utilisateur {
-
-    public Admin() {
-    }
-
-    public Admin(String firstName, String lastName, String email, String password) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-    }
-}
